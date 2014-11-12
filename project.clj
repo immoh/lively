aee13b17f71c741f37d6cd73b436a5b0291126f4
Immo Heikkinen <immo.heikkinen@ekahau.com>
2014-11-12 10:02:36 +0200

    Add cljs as dev dependency

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
+                                  [org.clojure/clojurescript "0.0-2371"]]}})

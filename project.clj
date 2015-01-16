fba91ff1347e7ed7f713a0d619d0e804193ff9b1
Immo Heikkinen <immo.heikkinen@gmail.com>
2015-01-16 10:38:16 +0200

    Bump CLJS version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
-                                  [org.clojure/clojurescript "0.0-2371"]]
+                                  [org.clojure/clojurescript "0.0-2665"]]
                    :plugins [[lein-cljsbuild "1.0.4"]]}}
   :cljsbuild {:builds {:main {:source-paths ["src"]
                               :compiler {:optimizations :none}}}})
